c5463757e78bc221315d9994596be47871d043c6
Michael <mkahn010@gmail.com>
2021-01-28 18:01:18 -0800

    Updated is_valid to check domain and added defragmentation of links

--- scraper.py
+++ scraper.py
@@ -1,21 +1,29 @@
 import re
+import lxml
 from urllib.parse import urlparse
+from urllib.parse import urldefrag
+
+valid_domains = "ics.uci.edu|cs.uci.edu|informatics.uci.edu|stat.uci.edu"
+
 
 def scraper(url, resp):
     links = extract_next_links(url, resp)
-    return [link for link in links if is_valid(link)]
+    return [urldefrag(link)[0] for link in links if is_valid(link)]
+
 
 def extract_next_links(url, resp):
-    # Implementation requred.
-    print(url)
+    # Implementation required.
     return list()
+
 
 def is_valid(url):
     try:
         parsed = urlparse(url)
+        # Check if valid http/https link
         if parsed.scheme not in set(["http", "https"]):
             return False
-        return not re.match(
+        # Check if not a web page
+        if re.match(
             r".*\.(css|js|bmp|gif|jpe?g|ico"
             + r"|png|tiff?|mid|mp2|mp3|mp4"
             + r"|wav|avi|mov|mpeg|ram|m4v|mkv|ogg|ogv|pdf"
@@ -23,8 +31,10 @@ def is_valid(url):
             + r"|data|dat|exe|bz2|tar|msi|bin|7z|psd|dmg|iso"
             + r"|epub|dll|cnf|tgz|sha1"
             + r"|thmx|mso|arff|rtf|jar|csv"
-            + r"|rm|smil|wmv|swf|wma|zip|rar|gz)$", parsed.path.lower())
-
+            + r"|rm|smil|wmv|swf|wma|zip|rar|gz)$", parsed.path.lower()):
+            return False
+        # Check if a valid domain
+        return re.search(rf"({valid_domains})", parsed.netloc)
     except TypeError:
         print("TypeError for ", parsed)
         raise
