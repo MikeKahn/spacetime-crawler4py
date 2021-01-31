34f275f5a60a1d4201e4738f1cfe07fec783ef01
Jirigesi <jirigesi@gmail.com>
2021-01-31 15:32:50 -0800

    finish Q1 and Q4, please review

--- scraper.py
+++ scraper.py
@@ -6,13 +6,53 @@ from urllib.parse import urldefrag
 from urllib.parse import urljoin
 from urllib.parse import parse_qs
 import datetime
+import os
 
 valid_domains = "ics.uci.edu|cs.uci.edu|informatics.uci.edu|stat.uci.edu"
 logging = True
 output = None
-
+#
+unique_pages = []
+subdomain_dic = {}
 
 def scraper(url, resp):
+    ####################################
+    # Get results for Q1 and Q4:
+    ####################################
+    if '#' in url:
+        url_no_fragment = url.split('#')[0]
+    else:
+        url_no_fragment = url
+    if url_no_fragment not in unique_pages:
+        # calulate the lines in "unique_pages.txt" for Q1
+        unique_pages.append(url_no_fragment)
+        # handle Q1 results
+        writepath = "unique_pages.txt"
+        mode = 'a' if os.path.exists(writepath) else 'w'
+        with open(writepath, mode) as a_file:
+            if mode == 'w':
+                 a_file.write(url_no_fragment)
+            else:
+                a_file.write("\n")
+                a_file.write(url_no_fragment)
+        # handle Q4 results
+        o = urlparse(url_no_fragment)
+        current_page_domian = o.scheme + '://' + o.netloc
+
+        if current_page_domian.endswith('.ics.uci.edu') and not current_page_domian.endswith('www.ics.uci.edu'):
+            if current_page_domian in subdomain_dic:
+                subdomain_dic[current_page_domian] += 1
+            else: 
+                subdomain_dic[current_page_domian] = 1
+
+        writepath2 = "question4.txt"
+
+        with open(writepath2, "w") as a_file:
+            for key, val in subdomain_dic.items():
+                a_file.write("\n")
+                a_file.write(key+ ', ' + str(val))
+    ####################################
+
     links = extract_next_links(url, resp)
     urls = []
     for link in links:
@@ -25,7 +65,7 @@ def scraper(url, resp):
         else:
             link = urljoin(link, parsed.path)
         if is_valid(link):
-            urls.append(urldefrag(link))
+            urls.append(urldefrag(link)[0])
     return urls
 
 
